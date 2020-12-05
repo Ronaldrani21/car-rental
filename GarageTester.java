0ba8385c451a526d15b76ed916e6b990d415229b
Ronaldrani21 <73988385+Ronaldrani21@users.noreply.github.com>
2020-12-05 09:31:37 +0300

    Create GarageTester.java

---
+++ GarageTester.java
@@ -0,0 +1,9 @@
+
+public class GarageTester {
+    public String getExample(){
+            Truck tata = new Truck("black",false);
+            
+            Garage BnG = new Garage();
+            return BnG.setVehicle(tata);
+}
+}
